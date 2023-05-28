f23769266dec7a9031bf1c6d017d45b7fd536f1f
Ash Griffiths <46426454+AshGriffiths@users.noreply.github.com>
2023-05-28 10:47:57 +0100

    Add log middleware

--- main.go
+++ main.go
@@ -1,8 +1,27 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"strconv"
 )
+
+type apiConfig struct {
+	fileServerHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits++
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) showHits(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	writer.WriteHeader(http.StatusOK)
+	writer.Write([]byte("Hits: " + strconv.Itoa(cfg.fileServerHits)))
+}
 
 func middlewareCors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -17,14 +36,24 @@ func middlewareCors(next http.Handler) http.Handler {
 	})
 }
 
+func middlewareLog(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s", r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
+	var apiCfg apiConfig
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/metrics", apiCfg.showHits)
 	mux.HandleFunc("/healthz", func(writer http.ResponseWriter, request *http.Request) {
 		writer.Header().Add("Content-Type", "text/plain; charset=utf-8")
-		writer.WriteHeader(200)
+		writer.WriteHeader(http.StatusOK)
 		writer.Write([]byte("OK"))
 	})
 	corsMux := middlewareCors(mux)
-	http.ListenAndServe(":8080", corsMux)
+	logMux := middlewareLog(corsMux)
+	http.ListenAndServe(":8080", logMux)
 }
